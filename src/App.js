82735b3ae6c96ab8ff2bb2f345eb372a1cd93986
MJBHjem <michael.bryson@hjem.ca>
2021-01-31 19:23:54 -0400

    finished assignment 2 on lists and conditional and click

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import CharComponent from './CharComponent/CharComponent'
 class App extends Component {
   constructor (props) {
     super(props)
+
     this.state = { userInput: '' }
     this.handleTextChg = this.handleTextChg.bind(this)
+    this.handleCharDelete = this.handleCharDelete.bind(this)
   }
 
   handleTextChg (event) {
     this.setState({ userInput: event.target.value })
   }
 
+  handleCharDelete (id) {
+    const uInput = this.state.userInput
+    const newUserInput = [...uInput.slice(0, id), ...uInput.slice(id+1)].join('')
+    this.setState({ userInput: newUserInput })
+  }
+
   render () {
     const uInput = this.state.userInput
     const validTxt = uInput.length > 4 ? 'text long enough' : 'text too short'
+    const charList = [...uInput].map((c, i) => {
+      return <CharComponent key={i} id={i} char={c} onCk={() => this.handleCharDelete(i)} />
+    })
+
     return (
       <div>
         <input
@@ -23,7 +35,8 @@ class App extends Component {
           value={this.state.userInput}
         />
         <p>{uInput} <span>{validTxt}</span></p>
-        <CharComponent />
+
+        {charList}
       </div>
     )
   }
--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const charComponent = () => {
+const charComponent = ({ char, onCk }) => {
   const style = {
     textAlign: 'center',
     display: 'inline-block',
@@ -9,7 +9,7 @@ const charComponent = () => {
     border: '1px solid black'
   }
 
-  return <div style={style}>Howdy</div>
+  return <div style={style} onClick={onCk}>{char}</div>
 }
 
 export default charComponent
